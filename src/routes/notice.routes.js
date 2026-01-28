8ce2e4065b7529ddfb49ad55984f952cabd792d2
nikhatchodhary <choudharynikhat74@gmail.com>
2026-01-28 22:04:05 +0530

    Add chat system

--- src/routes/notice.routes.js
+++ src/routes/notice.routes.js
@@ -6,20 +6,20 @@ import {
   deleteNotice,
 } from "../controllers/notice.controller.js";
 import {auth} from "../middlewares/auth.js"; // existing authentication middleware
-// import roleCheck from "../middlewares/roleCheck.js"; // restrict access to admin/coadmin
+import roleCheck from "../middlewares/roleCheck.js"; // restrict access to admin/coadmin
 
 const router = express.Router();
 
 // View all notices (any authenticated user)
 router.get("/", auth, getAllNotices);
 
-//  Create a new notice (admin/coadmin only)
-// router.post("/", auth, roleCheck, createNotice);
+ //Create a new notice (admin/coadmin only)
+router.post("/", auth, roleCheck, createNotice);
 
-// Update a notice (admin/coadmin only)
-// router.put("/:id", auth, roleCheck, updateNotice);
+//Update a notice (admin/coadmin only)
+router.put("/:id", auth, roleCheck, updateNotice);
 
-// Delete a notice (admin/coadmin only)
-// router.delete("/:id", auth, roleCheck, deleteNotice);
+ //Delete a notice (admin/coadmin only)
+ router.delete("/:id", auth, roleCheck, deleteNotice);
 
 export default router;
